023cd72a259e5107ce1835ba184e7f855bcd4e09
Ivan Nikolsky <enty8080@gmail.com>
2021-11-20 17:53:01 +0300

    Update session.py

--- pwny/session.py
+++ pwny/session.py
@@ -56,16 +56,18 @@ class PwnySession(Session, TelnetClient):
         return not self.client.terminated
 
     def send_command(self, command, output=False, timeout=10):
-        if len(command) > 1:
-            if isinstance(command[1], dict):
-                args = command[1]
+        commands = self.format_commands(command)
+
+        if len(commands) > 1:
+            if isinstance(commands[1], dict):
+                args = commands[1]
             else:
-                args = ' '.join(command[1:])
+                args = ' '.join(commands[1:])
         else:
             args = None
 
         command_data = json.dumps({
-            'cmd': command[0],
+            'cmd': commands[0],
             'args': args
         })
 
